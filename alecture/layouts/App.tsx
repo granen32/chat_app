d0feed014b3f9ac70a147140a7f79af8e7473ed0
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2023-02-15 19:00:42 +0900

    회원가입 설정

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import loadable from "@loadable/component";
+import { Route, Switch, Redirect } from "react-router-dom";
 
+const Login = loadable(() => import("@pages/Login"));
+const SignUp = loadable(() => import("@pages/SignUp"));
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <>
+      <Switch>
+        <Redirect exact path="/" to="/login" />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
+
+// pages - 서비스 페이지
+// components - 짜잘 컴포넌트
+// layouts - 공통 페이지
--- alecture/webpack.config.ts
+++ alecture/webpack.config.ts
@@ -1,5 +1,5 @@
-import path from 'path';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import path from "path";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import webpack, { Configuration as WebpackConfiguration } from "webpack";
 import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 
@@ -7,56 +7,57 @@ interface Configuration extends WebpackConfiguration {
   devServer?: WebpackDevServerConfiguration;
 }
 
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 const config: Configuration = {
-  name: 'sleact',
-  mode: isDevelopment ? 'development' : 'production',
-  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
+  name: "sleact",
+  mode: isDevelopment ? "development" : "production",
+  devtool: !isDevelopment ? "hidden-source-map" : "eval",
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    // 폴더구조
     alias: {
-      '@hooks': path.resolve(__dirname, 'hooks'),
-      '@components': path.resolve(__dirname, 'components'),
-      '@layouts': path.resolve(__dirname, 'layouts'),
-      '@pages': path.resolve(__dirname, 'pages'),
-      '@utils': path.resolve(__dirname, 'utils'),
-      '@typings': path.resolve(__dirname, 'typings'),
+      "@hooks": path.resolve(__dirname, "hooks"),
+      "@components": path.resolve(__dirname, "components"),
+      "@layouts": path.resolve(__dirname, "layouts"),
+      "@pages": path.resolve(__dirname, "pages"),
+      "@utils": path.resolve(__dirname, "utils"),
+      "@typings": path.resolve(__dirname, "typings"),
     },
   },
   entry: {
-    app: './client',
+    app: "./client",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
           presets: [
             [
-              '@babel/preset-env',
+              "@babel/preset-env",
               {
-                targets: { browsers: ['IE 10'] },
+                targets: { browsers: ["IE 10"] },
                 debug: isDevelopment,
               },
             ],
-            '@babel/preset-react',
-            '@babel/preset-typescript',
+            "@babel/preset-react",
+            "@babel/preset-typescript",
           ],
           env: {
             development: {
-              plugins: [require.resolve('react-refresh/babel')],
+              plugins: [require.resolve("react-refresh/babel")],
             },
           },
         },
-        exclude: path.join(__dirname, 'node_modules'),
+        exclude: path.join(__dirname, "node_modules"),
       },
       {
         test: /\.css?$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
@@ -67,17 +68,19 @@ const config: Configuration = {
       //   files: "./src/**/*",
       // },
     }),
-    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: isDevelopment ? "development" : "production",
+    }),
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: '/dist/',
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/dist/",
   },
   devServer: {
     historyApiFallback: true, // react router
     port: 3090,
-    devMiddleware: { publicPath: '/dist/' },
+    devMiddleware: { publicPath: "/dist/" },
     static: { directory: path.resolve(__dirname) },
   },
 };
